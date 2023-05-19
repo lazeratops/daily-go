f7e2a8ff145231e6a8cb926996f6f8dde1202bcd
Liza Shulyayeva <65890040+lazeratops@users.noreply.github.com>
2023-05-19 08:54:33 +0200

    Remove version reference for now

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
 mkdir local
-v=`go version | { read _ _ v _; echo ${v#go}; }`
-
 go test ./... -coverpkg=./... -coverprofile=local/coverage.out -covermode=atomic -count=1
-go tool cover -html=local/coverage.out -o local/coverage-$v.html
+go tool cover -html=local/coverage.out -o local/coverage.html
